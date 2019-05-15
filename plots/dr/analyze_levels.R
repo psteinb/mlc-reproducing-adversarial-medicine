37789e524d02f213b5a34b78073ba1a28c8af358
Peter Steinbach <steinbach@scionics.de>
2019-05-15 14:45:01 +0200

    added class inbalance plot

--- plots/dr/analyze_levels.R
+++ plots/dr/analyze_levels.R
@@ -8,11 +8,21 @@ df = df %>% bind_rows(ds)
 
 overview = df %>% group_by(level) %>% tally()
 
+overview$levels_fct = factor(overview$level)
+## grade 2 or worse
+overview = overview %>% mutate(malignant = ifelse(level <= 2,"healthy","malignant"))
+
+
+levels(overview$levels_fct)
+levels(overview$levels_fct) = c("No DR", "Mild", "Moderate", "Severe", "Proliferative DR")
+head(overview)
+
 repro = df %>% mutate(malignant = level <= 2) %>% group_by(malignant) %>%tally()
 head(repro)
 
-pl = ggplot(overview, aes(as.factor(level),n))
+pl = ggplot(overview, aes(levels_fct,n, fill=reorder(malignant, desc(malignant))))
 pl = pl + geom_bar(stat = "identity")
+pl = pl + scale_fill_discrete(name="Medical Status")
 pl = pl + xlab("Class Label")
 pl = pl + ylab("N(images)")
 pl = pl + theme_light()
