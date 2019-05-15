007eb7aeaf8d366f0befa11cb1ac382d86916cc7
Peter Steinbach <steinbach@scionics.de>
2019-05-15 14:16:46 +0200

    added overview

--- plots/dr/analyze_levels.R
+++ plots/dr/analyze_levels.R
@@ -1,10 +1,21 @@
 library(dplyr)
 library(readr)
+library(ggplot2)
 
 df = read_csv("trainLabels.csv") 
 ds = read_csv("sampleSubmission.csv")
 df = df %>% bind_rows(ds)
 
-df %>% group_by(level) %>% tally()
+overview = df %>% group_by(level) %>% tally()
 
-df %>% mutate(malignant = level <= 2) %>% group_by(malignant) %>%tally()
+repro = df %>% mutate(malignant = level <= 2) %>% group_by(malignant) %>%tally()
+head(repro)
+
+pl = ggplot(overview, aes(as.factor(level),n))
+pl = pl + geom_bar(stat = "identity")
+pl = pl + xlab("Class Label")
+pl = pl + ylab("N(images)")
+pl = pl + theme_light()
+
+ggsave("dr_overview.svg",pl)
+ggsave("dr_overview.png",pl)
